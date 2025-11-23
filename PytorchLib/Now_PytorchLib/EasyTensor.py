a1159d769306c0e289a47671b759326f749167b5
qwp_p <qwp20060309@outlook.com>
2025-11-23 20:30:07 +0800

    feat(pytorch): 添加多种张量创建和转换方法
    - 新增 Type_Tensor_or_tensor 函数，支持通过类型前缀和属性指定类型创建张量
    - 新增 Interval_tensor 函数，支持生成指定区间的张量
    - 新增 full_tensor 函数，支持生成全0/1/x及随机值的张量
    - 新增 random_tensor 函数，支持生成各种分布的随机数张量
    - 新增 perm_tensor 函数，支持对张量进行洗牌操作
    - 新增 TranAndAlgTensor.py 文件，包含张量类型转换、numpy转换、标量转换及运算功能
    - 添加 trans_type_tensor 函数，实现张量类型的改变
    - 添加 trans_np_tensor 函数，实现ndarray与tensor之间的转换
    - 添加 Scalar_to_Tensor 函数，实现标量与张量的转换
    - 添加 Alg_tensor 函数，实现张量的基本运算和矩阵乘法

--- PytorchLib/Now_PytorchLib/EasyTensor.py
+++ PytorchLib/Now_PytorchLib/EasyTensor.py
@@ -82,7 +82,130 @@ def Easy_Tensor():
     torch.float32
     """
 
+def Type_Tensor_or_tensor():
+    """
+    类型前缀和属性指定类型
+    :return:
+    """
+    x = torch.FloatTensor(2, 3, 4) # 形状生成
+    # LongTensor、DoubleTensor、IntTensor、ShortTensor、ByteTensor、HalfTensor、BoolTensor
+    print(x)
+    print(x.size())
+    print(x.dtype)
+    """
+    tensor([[[3.8707e+25, 1.7628e-42, 0.0000e+00, 0.0000e+00],
+         [0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00],
+         [0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00]],
+
+        [[0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00],
+         [0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00],
+         [0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00]]])
+    torch.Size([2, 3, 4])
+    torch.float32
+    """
+    x1 = torch.tensor([1, 2, 3], dtype=torch.float) # 内容生成
+    print(x1)
+    print(x1.size())
+    print(x1.dtype)
+    """
+    tensor([1., 2., 3.])
+    torch.Size([3])
+    torch.float32
+    """
+
+def Interval_tensor():
+    """
+    生成指定区间的tensor
+    :return:
+    """
+    x = torch.arange(0, 10, 2) # 0-10，步长为2，左闭右开
+    print(x)
+
+    x1 = torch.linspace(0, 10, 5) # 0-10，生成5个，左闭右闭
+    print(x1)
+
+    x2 = torch.logspace(0, 10, 5, base=2) # 0-10，生成5个，左闭右闭，底数是2
+    print(x2)
+
+def full_tensor():
+    """
+    生成全0/1/x的tensor
+    :return:
+    """
+    x = torch.zeros(2, 3, 4) # 生成全0的tensor
+    print(x)
+    x1 = torch.ones(2, 3, 4) # 生成全1的tensor
+    print(x1)
+    x2 = torch.full((2, 3, 4), 5) # 生成全5的tensor
+    print(x2)
+    x3 = torch.empty(2, 3, 4) # 随机生成指定大小的tensor
+    print(x3)
+
+    # 根据已知张量生成
+    x4 = torch.zeros_like(x) # 根据x生成全0的tensor
+    print(x4)
+    x5 = torch.ones_like(x) # 根据x生成全1的tensor
+    print(x5)
+    x6 = torch.full_like(x, 5) # 根据x生成全5的tensor
+    print(x6)
+    x7 = torch.empty_like(x) # 根据x生成随机的tensor
+    print(x7)
+
+    # 单位阵
+    x8 = torch.eye(3) # 单位阵
+    print(x8)
+    x9 = torch.eye(3, 4) # 3行4列
+    print(x9)
+    """
+    tensor([[1., 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.]])
+    tensor([[1., 0., 0., 0.],
+        [0., 1., 0., 0.],
+        [0., 0., 1., 0.]])
+    """
+
+def random_tensor():
+    """
+    生成随机数
+    :return:
+    """
+    x = torch.rand(2, 3, 4) # 均匀分布随机生成三维[0,1)的 tensor
+    print(x)
+    x1 = torch.randint(0, 10, (2, 3, 4)) # 均匀分布随机生成三维[0,10)的 int tensor
+    print(x1)
+    x2 = torch.randn(2, 3, 4) # 标准正态分布随机生成三维[-1,1)的 tensor
+    print(x2)
+    x3 = torch.normal(0, 1, (2, 3, 4)) # 正态分布随机生成三维均值为 0，方差为 1 的 tensor
+
+    # 根据已有张量生成
+    x4 = torch.rand_like(x)
+    print(x4)
+    x5 = torch.randint_like(x, 0, 10) # 根据已有张量生成，在[0,10)内
+    print(x5)
+    x6 = torch.randn_like(x) # 根据已有张量生成，标准正态分布
+
+def perm_tensor():
+    """
+    对tensor洗牌
+    :return:
+    """
+    x = torch.randperm(10) # 0-9，随机排序
+    print(x)
+    print(torch.random.initial_seed()) # 获取当前种子
+    torch.random.manual_seed(42) # 设置种子
+
 if __name__ == '__main__':
-    Easy_tensor()
+    # Easy_tensor()
     print("==========")
-    Easy_Tensor()
+    # Easy_Tensor()
+    print("==========")
+    # Type_Tensor_or_tensor()
+    print("==========")
+    # Interval_tensor()
+    print("==========")
+    # full_tensor()
+    print("==========")
+    # random_tensor()
+    print("==========")
+    # perm_tensor()
---
+++ PytorchLib/Now_PytorchLib/TranAndAlgTensor.py
@@ -0,0 +1,104 @@
+import torch
+import numpy as np
+
+def trans_type_tensor():
+    """
+    张量的改变类型
+    :return:
+    """
+    x = torch.tensor([1, 2, 3], dtype=torch.float)
+    print(x.dtype)
+    # 类型转换
+    x1 = x.type(torch.int)
+    print(x1.dtype)
+    # 或者直接调用函数
+    x2 = x.half()
+    print(x2.dtype)
+    # 再或者
+    x3 = x.to(torch.complex64) # 64位复数
+    print(x3.dtype)
+
+def trans_np_tensor():
+    """
+    ndarray与tensor的转换
+    :return:
+    """
+    # 设置打印选项
+    np.set_printoptions(precision=4)
+    torch.set_printoptions(precision=4)
+
+    x = np.array([1, 2, 3])
+    x1 = torch.from_numpy(x) # ndarray 转 tensor
+    print(x1.dtype)
+    x2 = x1.numpy() # tensor 转 ndarray
+    # 上述转换是浅拷贝，即 x 和 x2 都指向同一块内存，如果 x1 改变，x2 也会改变
+
+    # 取消内存共享
+    # x2 = x1.numpy().copy() # 创建新的数组
+    # x1 = torch.from_numpy(x2.copy()) # 创建新的张量
+
+def Scalar_to_Tensor():
+    """
+    标量与张量的转换
+    :return:
+    """
+    x = torch.tensor(10) # 创建张量
+    # x = torch.tensor([10]) 也可以
+    # x = torch.tensor([[10]]) # 也也可以
+    print(x)
+    # 转换为标量
+    print(x.item())
+
+def Alg_tensor():
+    """
+    张量的运算
+    :return:
+    """
+    x = torch.randint(10, 100, (2, 3))
+    print(x + 10)
+    print(x - 10)
+    print(-x)
+    print(x * 10)
+    print(x / 10)
+    print(x ** 2)
+    print(x // 10)
+
+    # 函数
+    print(x.add(10)) # 加
+    print(x.neg()) # 取负数
+    print(x.sub(10)) # 减
+    print(x.mul(10)) # 乘，哈达玛积，即对应位置相乘
+    print(x.div(10)) # 除
+    print(x.pow(2)) # 幂
+    print(x.sqrt()) # 平方根
+    print(x.exp()) # 指数
+
+    # 取代原对象数据（注意类型不一不可进行该操作）
+    # print(x.add_(10))  # 加
+    # print(x.neg_())  # 取负数
+    # print(x.sub_(10))  # 减
+    # print(x.mul_(10))  # 乘，哈达玛积，即对应位置相乘
+    # print(x.div_(10))  # 除
+    # print(x.pow_(2))  # 幂，可以传分数实现开方
+    # print(x.sqrt_())  # 平方根
+    # ...
+
+    # 矩阵乘法
+    x = torch.randint(10, 100, (2, 3))
+    y = torch.randint(10, 100, (3, 4))
+    print(x @ y) # 语法糖
+    print(torch.matmul(x, y)) # 多维矩阵乘法
+    print(torch.mm(x, y)) # 二维专用矩阵乘法
+
+if __name__ == '__main__':
+    """
+    转换和计算tensor
+    """
+    # trans_type_tensor()
+    print("==========")
+    # trans_np_tensor()
+    print("==========")
+    # Scalar_to_Tensor()
+    print("==========")
+    # Alg_tensor()
+    print("==========")
